d27dc89099ad603083f70a45ababb7401a95fb35
Kody Buss <kody.buss@gmail.com>
2016-03-24 18:55:17 -0500

    Mostly Textured and designed space track in the shape of an eight. Need to finish adding details and models from sketchup warehouse.

---
+++ Assets/Scripts/Rotate1.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate1 : MonoBehaviour {
+	public float speed = 4f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (0, -speed, speed);
+	}
+}
