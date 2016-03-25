85e119f6c8f346229eb4ca2904465e76886f5bdc
Kody Buss <kody.buss@gmail.com>
2016-03-25 15:20:36 -0500

    Finished Version submitted for grading.

---
+++ Assets/Scripts/Rotate2.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate2 : MonoBehaviour {
+	public float speed = 4f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (0, 0, speed);
+	}
+}
