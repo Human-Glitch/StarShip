74ffdae5248609b7c2ee12298e08d3288d8377c3
Kody Buss <kody.buss@gmail.com>
2016-04-14 18:58:57 -0500

    All dis shit be updated and ready to go yo.

---
+++ Assets/Scripts/playClangSound.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playClangSound : MonoBehaviour {
+	public GameObject player;
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "Player") {
+			audioSource.Play ();
+			Debug.Log ("Metal Sound");
+		}
+	}
+}
